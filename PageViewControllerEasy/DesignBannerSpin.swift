d442678b246918438400cdd5e8c4a9ee14692f9b
Victor Manuel Lagunas Franco <victor.lagunas@MacBook-Air-de-Soporte-2.local>
2017-12-17 15:09:28 -0600

    Commit actualización de código

--- PageViewControllerEasy/DesignBannerSpin.swift
+++ PageViewControllerEasy/DesignBannerSpin.swift
@@ -8,100 +8,103 @@
 
 import UIKit
 
-class DesignBannerSpin : UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
-
+class DesignBannerSpin : UIViewController{
+  
     var timer : Timer?
     var almacenadoTmp: Int = 0
     var timeIntervaloCursor : CGFloat = 4.0
     var stopAnimation : Bool  = false
-    let tituloPaginas = ["Cobra Con Tarjeta al Mejor Precio","Recibe Pagos Con Tarjeta","Usa Tu Dinero Como Quieras",""]
-    var images = ["IntroA","IntroB","IntroC","introD"]
+    let tituloPaginas = ["Perrito Peluchin","Perrito Capulino","Perrito solovino","Perrito Misha"]
+    var images = ["perrito1","perrito2","perrito3","perrito4"]
+  
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        
+        self.view.addGestureRecognizer(swipeRight)
+        self.view.addGestureRecognizer(swipeLeft)
+
+        changeImage(number: 0)
+        animation()
+        
+    }
     
+    @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                animationBack()
+            case UISwipeGestureRecognizerDirection.down:
+                print("Swiped down")
+            case UISwipeGestureRecognizerDirection.left:
+                animationNext()
+            case UISwipeGestureRecognizerDirection.up:
+                print("Swiped up")
+            default:
+                break
+            }
+        }
+    }
     
-    var pageViewController: UIPageViewController!
-    
-    @IBAction func swipeLeft(sender: AnyObject) {
-        print("SWipe left")
+   
+    func changeImage (number : Int) {
+        
+        self.backgroundImage?.image = UIImage(named: self.images[number])
+        self.backgroundImage?.alpha = 0.1
+        self.titleLabel?.text = self.tituloPaginas[number]
+        self.pageControl.currentPage = number
+        self.titleLabel?.alpha = 0.1
+        UIView.animate(withDuration: 2.0, animations: { () -> Void in
+            self.titleLabel?.alpha = 1.0
+            self.backgroundImage?.alpha = 1.0
+        })
     }
     
     func animation(){
         
         timer = Timer.scheduledTimer(timeInterval: TimeInterval(timeIntervaloCursor), target: self, selector: #selector(self.updateAnimation), userInfo: nil, repeats: true)
     }
-    func updateAnimation(){
-        print("almacenado tmp: \(almacenadoTmp)")
-            if almacenadoTmp >= 3{
-                almacenadoTmp = 0
-            }else{
-                almacenadoTmp += 1
-            }
-    
-        let pageContentViewController = self.viewControllerAtIndex(index: almacenadoTmp)
-        self.pageViewController.setViewControllers([pageContentViewController!], direction: UIPageViewControllerNavigationDirection.forward, animated: true, completion: nil)
+    func animationBack(){
         
-    }
-
-    //Stop animation cursor
-    func stopAnimationCursor () {
-        stopAnimation = true
-    
-    }
-    
-    //Start animation cursor
-    func startAnimationCursor() {
-        stopAnimation = false
-        
-    }
-
-    @IBAction func swiped(sender: AnyObject) {
-        
-        self.pageViewController.view .removeFromSuperview()
-        self.pageViewController.removeFromParentViewController()
-        reset()
-    }
-    
-    func reset() {
-        /* Getting the page View controller */
-        pageViewController = self.storyboard?.instantiateViewController(withIdentifier: "PageViewController") as! UIPageViewController
-       self.pageViewController.dataSource = self
-        let pageContentViewController = self.viewControllerAtIndex(index: 0)
-        
-        self.pageViewController.setViewControllers([pageContentViewController!], direction: UIPageViewControllerNavigationDirection.forward, animated: true, completion: nil)
-
-        /* We are substracting 30 because we have a start again button whose height is 30*/
-        self.pageViewController.view.frame = CGRect(x: 0,y: 0,width: self.view.frame.width,height: self.view.frame.height)
-        self.addChildViewController(pageViewController)
-        self.view.addSubview(pageViewController.view)
-        self.pageViewController.didMove(toParentViewController: self)
-    }
-    
-    func viewControllerAtIndex(index : Int) -> UIViewController? {
-        var number = index
-        if((self.tituloPaginas.count == 0) || (index >= self.tituloPaginas.count)) {
-            return nil
+        if almacenadoTmp <= 0{
+            almacenadoTmp = tituloPaginas.count-1
+        }else{
+            almacenadoTmp -= 1
         }
-        let pageContentViewController = self.storyboard?.instantiateViewController(withIdentifier: "InicioSesionBanner") as! InicioSesionBannerSpinUIViewController
-        if number <= -1 {
-            number = 0
-        }
-            pageContentViewController.imageName = images[number]
-            pageContentViewController.titleText = tituloPaginas[number]
-            pageContentViewController.pageIndex = number
-        
-       
-        return pageContentViewController
-    }
-
-    @IBAction func start(sender: AnyObject) {
-        let pageContentViewController = self.viewControllerAtIndex(index: 0)
-        self.pageViewController.setViewControllers([pageContentViewController!], direction: UIPageViewControllerNavigationDirection.forward, animated: true, completion: nil)
+        changeImage (number : almacenadoTmp)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        reset()
-        animation()
+    func animationNext(){
+        if almacenadoTmp >= tituloPaginas.count-1{
+            almacenadoTmp = 0
+        }else{
+            almacenadoTmp += 1
+        }
+        changeImage (number : almacenadoTmp)
+    }
+    
+    
+    @objc func updateAnimation(){
+        
+        print("Splash timer: \(almacenadoTmp)")
+        if almacenadoTmp >= tituloPaginas.count-1{
+            almacenadoTmp = 0
+        }else{
+            almacenadoTmp += 1
+        }
+        changeImage (number : almacenadoTmp)
     }
     
     override func didReceiveMemoryWarning() {
@@ -109,37 +112,9 @@ class DesignBannerSpin : UIViewController, UIPageViewControllerDataSource, UIPag
         // Dispose of any resources that can be recreated.
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-       
-        var index = (viewController as! InicioSesionBannerSpinUIViewController).pageIndex!
-        
-        index -= 1
-        almacenadoTmp = index
-        
-        if index == 0 {
-            return nil
-        }
-        
-        return self.viewControllerAtIndex(index: almacenadoTmp)
-    }
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        
-        var index = (viewController as! InicioSesionBannerSpinUIViewController).pageIndex!
-        index += 1
-        almacenadoTmp = index
-        if index == self.images.count{
-            return nil
-        }
-        return self.viewControllerAtIndex(index: almacenadoTmp)
-    }
-  
-    
-    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
-    return tituloPaginas.count
-    }
-    
-    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-    return 0
+    func finishAnimation(){
+        timer?.invalidate()
+        timer = nil
     }
     
 }
